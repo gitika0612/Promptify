24a65ef96931ca274bcac54bbec71db9e5c0a447
gitika0612 <67464641+gitika0612@users.noreply.github.com>
2026-01-27 18:52:14 +0530

    Fix duplicate exports and serverless entry

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+
 import connectDB from "./configs/db.js";
+
 import userRouter from "./routes/userRoutes.js";
 import chatRouter from "./routes/chatRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
 import creditRouter from "./routes/creditRoutes.js";
+
 import { stripeWebhooks } from "./controllers/webHooks.js";
 
 const app = express();
@@ -31,12 +34,6 @@ app.use("/api/user", userRouter);
 app.use("/api/chat", chatRouter);
 app.use("/api/message", messageRouter);
 app.use("/api/credit", creditRouter);
-
-const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running at port ${PORT}`);
-// });
 
 app.use((err, req, res, next) => {
   console.error("SERVER ERROR:", err);
